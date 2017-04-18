df07fce6f41fa34bac0ad2cbb1d7618059f8c28d
Sarata Bartlomiej <Bartus@mbp-sarata.home>
2017-04-18 23:36:00 +0200

    Initial Commit

---
+++ SW_PROJECT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SW_PROJECT
+//
+//  Created by Sarata Bartlomiej on 18.04.2017.
+//  Copyright © 2017 Sarata Bartlomiej. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
