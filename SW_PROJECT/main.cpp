9b7bc4402bc9df3540c206202bc788ddde4e0200
Sarata Bartlomiej <Bartus@mbp-sarata.home>
2017-04-19 19:58:55 +0200

    SNAPSHOTFour

--- SW_PROJECT/main.cpp
+++ SW_PROJECT/main.cpp
@@ -8,12 +8,27 @@
 
 #include <iostream>
 #include <systemc>
+#include "moduleOne.cpp"
+
 using namespace std;
 using namespace sc_core;
 
 int sc_main(int argc, char *argv[]) {
-    cout << "Hello, World!\n";
+    
+    sc_signal<float> we_a;
+    sc_signal<float> out;
+    
+    //dane wejsciowe
+    we_a= 32;
+    
+    wartosc wart("");
+    
+    wart.wejscie_a(we_a);
+    
+    wart.Wyjscie(out);
+    
     sc_start();
+    sc_stop();
     return 0;
 }
 
---
+++ SW_PROJECT/moduleOne.cpp
@@ -0,0 +1,44 @@
+//
+//  moduleOne.cpp
+//  SW_PROJECT
+//
+//  Created by Sarata Bartlomiej on 19.04.2017.
+//  Copyright © 2017 Sarata Bartlomiej. All rights reserved.
+//
+
+#include "moduleOne.hpp"
+#include <systemc>
+using namespace sc_core;
+using namespace std;
+
+struct synchro{
+    sc_signal<float> wartosc;
+    sc_event zdarzenie;
+};
+
+SC_MODULE(wartosc){
+    sc_in<float> wejscie_a;
+    sc_out<float> Wyjscie;
+    synchro pobranoJeden;
+    synchro koniec1;
+    
+    void pierwszy_prog(){
+        cout << "30 stopni" << endl;
+        pobranoJeden.wartosc.write(wejscie_a.read());
+        pobranoJeden.zdarzenie.notify(0,SC_NS);
+        cout << "Minuty = " << wejscie_a.read() << endl;
+    }
+    
+    void task_1(){
+        wait(pobranoJeden.zdarzenie);
+        float temp = pobranoJeden.wartosc.read();
+        koniec1.wartosc.write(temp);
+        koniec1.zdarzenie.notify(0,SC_NS);
+        cout << "TASK 1: Program 30 stopni " <<  temp << " minuty" << endl;
+    }
+    
+    SC_CTOR(wartosc){
+        SC_THREAD(pierwszy_prog);
+        SC_THREAD(task_1);
+    }
+};
